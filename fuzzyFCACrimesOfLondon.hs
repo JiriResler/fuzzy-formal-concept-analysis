bcc956b1e4270de192db8ac2d3b4a72e15683ee4
JiriResler <5223265@upjs.sk>
2019-12-16 11:29:16 +0100

    Outputs for the years 2015 and 2016 generated

--- fuzzyFCACrimesOfLondon.hs
+++ fuzzyFCACrimesOfLondon.hs
@@ -4,8 +4,8 @@ import System.IO
 main :: IO ()
 main = do 
 ---------------------------------------------------
-  let inputTextFile = "input/crimes_2014.txt"
-  let outputTextFile = "output/clusters_2014.txt"
+  let inputTextFile = "input/crimes_2016.txt"
+  let outputTextFile = "output/clusters_2016.txt"
 ---------------------------------------------------
   inputFile <- readFile inputTextFile
   let linesInput = separateLines inputFile
